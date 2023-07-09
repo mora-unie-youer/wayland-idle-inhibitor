b1c894ae68e9cce4e2d2ffcaf8be56e50c8020f4
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-07-09 14:48:48 +0000

    Added `clap` for better CLI

---
+++ src/daemon.rs
@@ -0,0 +1,156 @@
+use wayland_client::{
+    protocol::{wl_compositor, wl_registry, wl_surface},
+    Connection, Dispatch, QueueHandle,
+};
+use wayland_protocols::wp::idle_inhibit::zv1::client::{
+    zwp_idle_inhibit_manager_v1, zwp_idle_inhibitor_v1,
+};
+
+#[derive(Debug)]
+struct IdleInhibitorDaemon {
+    queue_handle: QueueHandle<Self>,
+
+    base_surface: Option<wl_surface::WlSurface>,
+    idle_inhibit_manager: Option<zwp_idle_inhibit_manager_v1::ZwpIdleInhibitManagerV1>,
+    idle_inhibitor: Option<zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1>,
+}
+
+impl IdleInhibitorDaemon {
+    fn create_idle_inhibitor(&self) -> zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1 {
+        if self.base_surface.is_none() || self.idle_inhibit_manager.is_none() {
+            panic!("Surface and idle inhibit manager were not initialized");
+        }
+
+        let surface = self.base_surface.as_ref().unwrap();
+        let idle_inhibit_manager = self.idle_inhibit_manager.as_ref().unwrap();
+        idle_inhibit_manager.create_inhibitor(surface, &self.queue_handle, ())
+    }
+
+    fn toggle_idle_inhibit(&mut self) {
+        if let Some(idle_inhibitor) = &self.idle_inhibitor {
+            idle_inhibitor.destroy();
+            self.idle_inhibitor = None;
+        } else {
+            self.idle_inhibitor = Some(self.create_idle_inhibitor());
+        }
+    }
+
+    fn enable_idle_inhibit(&mut self) {
+        if self.idle_inhibitor.is_none() {
+            self.idle_inhibitor = Some(self.create_idle_inhibitor());
+        }
+    }
+
+    fn disable_idle_inhibit(&mut self) {
+        if let Some(idle_inhibitor) = &self.idle_inhibitor {
+            idle_inhibitor.destroy();
+            self.idle_inhibitor = None;
+        }
+    }
+}
+
+impl Dispatch<wl_registry::WlRegistry, ()> for IdleInhibitorDaemon {
+    fn event(
+        state: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &Connection,
+        qh: &QueueHandle<Self>,
+    ) {
+        if let wl_registry::Event::Global {
+            name, interface, ..
+        } = event
+        {
+            match &interface[..] {
+                "wl_compositor" => {
+                    let compositor =
+                        registry.bind::<wl_compositor::WlCompositor, _, _>(name, 1, qh, ());
+                    let surface = compositor.create_surface(qh, ());
+                    state.base_surface = Some(surface);
+                }
+                "zwp_idle_inhibit_manager_v1" => {
+                    state.idle_inhibit_manager = Some(registry.bind(name, 1, qh, ()));
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Dispatch<wl_compositor::WlCompositor, ()> for IdleInhibitorDaemon {
+    fn event(
+        _: &mut Self,
+        _: &wl_compositor::WlCompositor,
+        _: wl_compositor::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        todo!()
+    }
+}
+
+impl Dispatch<wl_surface::WlSurface, ()> for IdleInhibitorDaemon {
+    fn event(
+        _: &mut Self,
+        _: &wl_surface::WlSurface,
+        _: wl_surface::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        todo!()
+    }
+}
+
+impl Dispatch<zwp_idle_inhibit_manager_v1::ZwpIdleInhibitManagerV1, ()> for IdleInhibitorDaemon {
+    fn event(
+        _: &mut Self,
+        _: &zwp_idle_inhibit_manager_v1::ZwpIdleInhibitManagerV1,
+        _: zwp_idle_inhibit_manager_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        todo!()
+    }
+}
+
+impl Dispatch<zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1, ()> for IdleInhibitorDaemon {
+    fn event(
+        _: &mut Self,
+        _: &zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1,
+        _: zwp_idle_inhibitor_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        todo!()
+    }
+}
+
+pub fn start_daemon() {
+    let conn = Connection::connect_to_env().expect("Couldn't connect to Wayland socket");
+    let display = conn.display();
+
+    let mut event_queue = conn.new_event_queue();
+    let qh = event_queue.handle();
+    let _registry = display.get_registry(&qh, ());
+
+    let mut state = IdleInhibitorDaemon {
+        queue_handle: qh,
+
+        base_surface: None,
+        idle_inhibit_manager: None,
+        idle_inhibitor: None,
+    };
+
+    // Initializing Wayland client
+    event_queue.roundtrip(&mut state).unwrap();
+
+    state.enable_idle_inhibit();
+    loop {
+        event_queue.blocking_dispatch(&mut state).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,151 +1,37 @@
-use wayland_client::{
-    protocol::{wl_compositor, wl_registry, wl_surface},
-    Connection, Dispatch, QueueHandle,
-};
-use wayland_protocols::wp::idle_inhibit::zv1::client::{
-    zwp_idle_inhibit_manager_v1, zwp_idle_inhibitor_v1,
-};
+use clap::{Parser, Subcommand};
 
-#[derive(Debug)]
-struct HyprlandIdleInhibitor {
-    queue_handle: QueueHandle<Self>,
+mod daemon;
 
-    base_surface: Option<wl_surface::WlSurface>,
-    idle_inhibit_manager: Option<zwp_idle_inhibit_manager_v1::ZwpIdleInhibitManagerV1>,
-    idle_inhibitor: Option<zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1>,
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
 }
 
-impl HyprlandIdleInhibitor {
-    fn create_idle_inhibitor(&self) -> zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1 {
-        if self.base_surface.is_none() || self.idle_inhibit_manager.is_none() {
-            panic!("Surface and idle inhibit manager were not initialized");
-        }
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Daemon mode
+    Daemon,
 
-        let surface = self.base_surface.as_ref().unwrap();
-        let idle_inhibit_manager = self.idle_inhibit_manager.as_ref().unwrap();
-        idle_inhibit_manager.create_inhibitor(surface, &self.queue_handle, ())
-    }
+    /// Enable idle inhibitor
+    Enable,
 
-    fn toggle_idle_inhibit(&mut self) {
-        if let Some(idle_inhibitor) = &self.idle_inhibitor {
-            idle_inhibitor.destroy();
-            self.idle_inhibitor = None;
-        } else {
-            self.idle_inhibitor = Some(self.create_idle_inhibitor());
-        }
-    }
+    /// Disable idle inhibitor
+    Disable,
 
-    fn enable_idle_inhibit(&mut self) {
-        if self.idle_inhibitor.is_none() {
-            self.idle_inhibitor = Some(self.create_idle_inhibitor());
-        }
-    }
-
-    fn disable_idle_inhibit(&mut self) {
-        if let Some(idle_inhibitor) = &self.idle_inhibitor {
-            idle_inhibitor.destroy();
-            self.idle_inhibitor = None;
-        }
-    }
-}
-
-impl Dispatch<wl_registry::WlRegistry, ()> for HyprlandIdleInhibitor {
-    fn event(
-        state: &mut Self,
-        registry: &wl_registry::WlRegistry,
-        event: wl_registry::Event,
-        _: &(),
-        _: &Connection,
-        qh: &QueueHandle<Self>,
-    ) {
-        if let wl_registry::Event::Global {
-            name, interface, ..
-        } = event
-        {
-            match &interface[..] {
-                "wl_compositor" => {
-                    let compositor =
-                        registry.bind::<wl_compositor::WlCompositor, _, _>(name, 1, qh, ());
-                    let surface = compositor.create_surface(qh, ());
-                    state.base_surface = Some(surface);
-                }
-                "zwp_idle_inhibit_manager_v1" => {
-                    state.idle_inhibit_manager = Some(registry.bind(name, 1, qh, ()));
-                }
-                _ => {}
-            }
-        }
-    }
-}
-
-impl Dispatch<wl_compositor::WlCompositor, ()> for HyprlandIdleInhibitor {
-    fn event(
-        _: &mut Self,
-        _: &wl_compositor::WlCompositor,
-        _: wl_compositor::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<Self>,
-    ) {
-        todo!()
-    }
-}
-
-impl Dispatch<wl_surface::WlSurface, ()> for HyprlandIdleInhibitor {
-    fn event(
-        _: &mut Self,
-        _: &wl_surface::WlSurface,
-        _: wl_surface::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<Self>,
-    ) {
-        todo!()
-    }
-}
-
-impl Dispatch<zwp_idle_inhibit_manager_v1::ZwpIdleInhibitManagerV1, ()> for HyprlandIdleInhibitor {
-    fn event(
-        _: &mut Self,
-        _: &zwp_idle_inhibit_manager_v1::ZwpIdleInhibitManagerV1,
-        _: zwp_idle_inhibit_manager_v1::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<Self>,
-    ) {
-        todo!()
-    }
-}
-
-impl Dispatch<zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1, ()> for HyprlandIdleInhibitor {
-    fn event(
-        _: &mut Self,
-        _: &zwp_idle_inhibitor_v1::ZwpIdleInhibitorV1,
-        _: zwp_idle_inhibitor_v1::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<Self>,
-    ) {
-        todo!()
-    }
+    /// Toggle idle inhibitor
+    Toggle,
 }
 
 fn main() {
-    let conn = Connection::connect_to_env().expect("Couldn't connect to Wayland socket");
-    let display = conn.display();
+    let cli = Cli::parse();
+    dbg!(&cli);
 
-    let mut event_queue = conn.new_event_queue();
-    let qh = event_queue.handle();
-    let _registry = display.get_registry(&qh, ());
-
-    let mut state = HyprlandIdleInhibitor {
-        queue_handle: qh,
-
-        base_surface: None,
-        idle_inhibit_manager: None,
-        idle_inhibitor: None,
-    };
-
-    // Initializing Wayland client
-    event_queue.roundtrip(&mut state).unwrap();
+    match cli.command {
+        Command::Daemon => daemon::start_daemon(),
+        Command::Enable => println!("Enable"),
+        Command::Disable => println!("Disable"),
+        Command::Toggle => println!("Toggle"),
+    }
 }
